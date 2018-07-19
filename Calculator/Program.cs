0d4523435fef91727552f281ccf17820a9f4179d
Michael Zöhling <michael.zoehling@outlook.com>
2018-07-19 11:21:02 +0200

    Load Assemblies with Reflection

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Calculator.Models;
@@ -10,8 +13,17 @@ namespace Calculator
         // Bootstrapping
         static void Main(string[] args)
         {
-            var parser = new Parser();
-            //var calc = new ModularCalculator(new Add(), new Multiplication());
+            foreach (string file in Directory.GetFiles(Path.Combine(Environment.CurrentDirectory,"PlugIns")))
+                if(Path.GetExtension(file) == ".dll")
+                    Assembly.LoadFile(file);
+
+            var allTypesWhichImplementICalculationOperation = AppDomain.CurrentDomain.GetAssemblies()
+                                                                       .SelectMany(assembly => assembly.GetTypes())
+                                                                       .Where(x => typeof(ICalculationOperation).IsAssignableFrom(x) && x.IsInterface == false && x.IsAbstract == false)
+                                                                       .ToArray();
+
+            var parser = new RegexParser();
+            var calc = new ModularCalculator( allTypesWhichImplementICalculationOperation.Select(x => (ICalculationOperation)Activator.CreateInstance(x)).ToArray() );
             new ConsoleUI(parser,calc).Start();
         }
     }
