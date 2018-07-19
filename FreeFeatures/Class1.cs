a28debf06acde786611e22168c70f36a591d3175
Michael Zöhling <michael.zoehling@outlook.com>
2018-07-19 11:07:40 +0200

    Created FreeFeatures Lib

---
+++ FreeFeatures/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeFeatures
+{
+    public class Add : ICalculationOperation
+    {
+        public char Operator => '+';
+
+        public int Calculate(int v1, int v2) => v1 + v2;
+    }
+    public class Sub : ICalculationOperation
+    {
+        public char Operator => '-';
+
+        public int Calculate(int v1, int v2) => v1 - v2;
+    }
+}
